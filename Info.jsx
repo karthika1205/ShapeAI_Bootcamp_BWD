62dde8c3df5487f992f2e6bc78927c48a1dd3b26
karthika1205 <61447767+karthika1205@users.noreply.github.com>
2021-06-27 22:56:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>
+        <u>Javascript and React.js</u>
+      </h1>
+      <p>A Basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
---
+++ Info2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Info1() {
+  return (
+    <div className="note">
+      <h1>
+        <u>Python and data vizualization</u>
+      </h1>
+      <p>Free 7 days long bootcamp</p>
+    </div>
+  );
+}
+export default Info1;
